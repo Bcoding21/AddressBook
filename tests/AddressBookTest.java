5e6dacd528b5c9b6c965e98de32f2253a499cc88
Brandon <brandon.cole@bison.howard.edu>
2018-04-05 12:00:54 -0400

    Added test case code

--- tests/AddressBookTest.java
+++ tests/AddressBookTest.java
@@ -1,5 +1,61 @@
+import org.junit.Test;
+
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 public class AddressBookTest {
 
+    private Contact contact = new Contact("Brandon", "Cole", "240-374-2961");
+    private Address address = new Address("10731 Castleton Way",
+            "Upper Marlboro", "Maryland", 20774);
+    private Entry entry = new Entry(contact, address);
+
+    private Address address2 = new Address("2225 4th Street", "Washington D.C",
+            "Washington D.C", 20005);
+    private Contact contact2 = new Contact("John", "Smith", "240-453-8795");
+    private Entry entry2 = new Entry(contact2, address2);
+
+    @Test
+    public void remove() {
+        AddressBook book = new AddressBook();
+        book.add(entry);
+        assertTrue(book.remove(entry));
+    }
+
+    @Test
+    public void add() {
+        AddressBook book = new AddressBook();
+        book.add(entry);
+        Set<Entry> entries = book.getEntries();
+        assertTrue(entries.contains(entry));
+    }
+
+    @Test
+    public void orderByZip() {
+        Set<Entry> entrySet = new TreeSet<>(new Entry.zipComparator());
+        entrySet.add(entry);
+        entrySet.add(entry2);
+
+        List<Entry> actual = new ArrayList<>(entrySet);
+
+        List<Entry> expected = new ArrayList<>();
+        expected.add(entry2);
+        expected.add(entry);
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    public void orderByLastName() {
+        Set<Entry> entrySet = new TreeSet<>(new Entry.lastNameComparator());
+        entrySet.add(entry2);
+        entrySet.add(entry);
+
+        List<Entry> actual = new ArrayList<>(entrySet);
+
+        List<Entry> expected = new ArrayList<>();
+        expected.add(entry);
+        expected.add(entry2);
+        assertTrue(actual.equals(expected));
+    }
 }
--- tests/MenuTest.java
+++ tests/MenuTest.java
@@ -1,5 +1,41 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MenuTest {
 
+    @Test
+    public void open() {
+        Menu menu = new Menu();
+        AddressBook book = menu.open("TestBook.bin");
+        assertTrue(book.equals(menu.getAddressBook()));
+    }
+
+    @Test
+    public void save() {
+        Menu menu = new Menu();
+        menu.createNew();
+        menu.save();
+        assertTrue(menu.getFile().exists());
+    }
+
+    @Test
+    public void saveAs() {
+    }
+
+    @Test
+    public void createNew() {
+        Menu menu = new Menu();
+        AddressBook book = menu.createNew();
+        assertTrue(book == menu.getAddressBook());
+    }
+
+    @Test
+    public void close() {
+        Menu menu = new Menu();
+        menu.createNew();
+        save();
+        menu.close();
+        assertTrue(menu.getAddressBook() == null && menu.getFile() == null);
+    }
 }
